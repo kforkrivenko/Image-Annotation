b6902ebe200ffb63fffb283f79fadf89ba0d5d32
krivenkodm <krivenko.da@phystech.edu>
2025-06-17 11:14:57 +0300

    Оптимизация

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from pathlib import Path
-import os
-from ui.app import ImageAnnotationApp
-import sys
 from utils.paths import *
+from ui.app import ImageAnnotationApp
+from pathlib import Path
+
+
+def initialize_heavy_components():
+    import torch
+    import cv2
+    from ultralytics import YOLO
+    from sklearn.model_selection import train_test_split
+    from PIL import Image, ImageDraw, ImageFont
+    print("Heavy components initialized.")
 
 
 def prepare_env():
@@ -11,7 +18,9 @@ def prepare_env():
     (DATA_DIR / "annotated_dataset").mkdir(exist_ok=True)
 
 
+
 if __name__ == "__main__":
     prepare_env()
     app = ImageAnnotationApp()
+    app.root.after(100, initialize_heavy_components)
     app.run()
